7f35bf6a9c196018f0e518963345df1971e52bee
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-07 13:39:58 +0500

    Create a person class
    In this commit
    - Add a person class with instance variables and public and private  methods.
    - Include getters and setters.

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+    def initialize(parent_permission: true, age, name = "Unknown")
+      @id = Random.rand(1..1000)
+      @name = name
+      @age = age
+    end
+
+    attr_accessor :name, :age
+    attr_reader :id
+
+    def is_of_age?
+        @age >= 18
+    end
+
+    def can_use_services?
+        is_of_age? || parent_permission?
+    end
+
+    private :is_of_age?
+  end
