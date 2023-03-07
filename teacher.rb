7618d6fa5ef2a2516d44daa9f68e4040c6171fd1
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-07 14:10:35 +0500

    Remove Linter errors.

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,11 @@
 require './person'
- 
 class Teacher < Person
-    def initialize(parent_permission: true, age, name = 'Unknown', specialization)
-        super(age, name, parent_permission)
-        @specialization=specialization
-    end
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
