03d52a2af7aa3b034b6f972c3899817616d834a3
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-08 19:29:27 +0500

    Create the has-many/belongs-to relationship between Classroom and Student.

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class Classroom
+  attr_reader :students
+  attr_accessor :label
+
   def initialize(label)
     @label = label
+    @students = []
   end
-  attr_accessor :label
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
 end
